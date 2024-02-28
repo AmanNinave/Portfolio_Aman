9b90a493dcc27e86f902c3db10b5d89eccb0544a
AmanNinave <amanninave987@gmail.com>
2024-02-29 01:00:45 +0530

    navbar structure created

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import "./Styles/Navbar.css"
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='header'>
+
+
+      <span className="logo"><i className="fab fa-node-js"></i> Aman</span>
+
+      <div id="menu" class="fas fa-bars"></div>
+
+      <nav className="navbar">
+        <ul>
+          <li><button className="active" > Home </button> </li>
+          <li><button > About </button> </li>
+          <li><button > Skills </button> </li>
+          <li><button > Education </button> </li>
+          <li><button > Projects </button> </li>
+          <li><button > Experience </button> </li>
+          <li><button > Contact </button> </li>
+        </ul>  
+          
+      </nav>
+
+      
+    </div>
   )
-}
+} 
