6dc254ef4c5a19b5b9b17d7c0ff3d8324c16c97c
AmanNinave <amanninave987@gmail.com>
2024-02-29 18:55:12 +0530

    Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Home/>
       
       
     </>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,103 @@
-import React from 'react'
+import React ,{useState , useEffect , useMemo } from "react";
+import "./Styles/Home.css";
+import Profile from "./../../public/vite.svg";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { particlesObject } from "./Config/particles.config";
 
 export default function Home() {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      
+      await loadSlim(engine);
+
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => (particlesObject),
+    [],
+  );
+
+
   return (
-    <div>Home</div>
-  )
+    <section className="home" id="home">
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+      <div className="content">
+        <h3>
+          Hi There,
+          <br /> I'm Aman <span>Ninave</span>
+        </h3>
+        <p>
+          i am  <span className="typing-text">Full-Stack Developer</span>
+        </p>
+
+        <button class="btn">
+          <span>Know More About Me </span>
+          <i class="fas fa-arrow-circle-up"></i>
+        </button>
+
+        <div className="socials">
+          <ul className="social-icons">
+            <li>
+              <button
+                className="linkedin"
+                href="https://www.linkedin.com/in/aman-ninave-61890a1aa/"
+                target="_blank"
+              >
+                <i className="fab fa-linkedin"></i>
+              </button>
+            </li>
+            <li>
+              <button
+                className="github"
+                href="https://github.com/AmanNinave"
+                target="_blank"
+              >
+                <i className="fab fa-github"></i>
+              </button>
+            </li>
+            <li>
+              <button
+                className="instagram"
+                href="https://www.instagram.com/amanninave987/"
+              >
+                <i className="fab fa-instagram" target="_blank"></i>
+              </button>
+            </li>
+            <li>
+              <button
+                className="gmail"
+                href="mailto:amanninave987@gmail.com "
+                target="_blank"
+              >
+                <i className="fas fa-envelope"></i>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="image">
+        <img
+          draggable="false"
+          className="tilt"
+          src={Profile}
+          alt="Aman's Image"
+        />
+      </div>
+    </section>
+  );
 }
