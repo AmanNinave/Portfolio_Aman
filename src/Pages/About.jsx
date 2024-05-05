11ae02cb0b64a0545a0bfb7f85e6e5bd924519ca
AmanNinave <amanninave987@gmail.com>
2024-05-05 12:02:08 +0530

    experiance & resume update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -37,7 +37,7 @@ export default function About() {
                 </div>
                 
                 <div className="resumebtn">
-                    <a href="https://drive.google.com/file/d/18eopvpn2KOoiqy7qVN1KZqylVlpgGohO/view?usp=sharing" className="btn"><span>Resume </span>
+                    <a href="https://drive.google.com/file/d/1I-VZfo8UThbtMn0-22Wb3RxcM3MdsK1z/view?usp=sharing" className="btn"><span>Resume </span>
                         <i className="fas fa-chevron-right"></i>
                     </a>
                 </div>
